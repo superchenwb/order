d7f6c85020378259d7f54f752e8357847308dfc3
superchenwb <chen1990wb@163.com>
2019-08-17 17:46:04 +0800

    第三章第六节数据准备

--- src/model/data.ts
+++ src/model/data.ts
@@ -30,6 +30,92 @@ const careerList: ICareer[] = [
   }
 ];
 
+// 家居维修职业的品类
+const serviceCategoryList = [
+  {
+    id: '0',
+    title: '家具',
+  },
+  {
+    id: '2',
+    title: '灯具',
+  },
+  {
+    id: '3',
+    title: '卫浴洁具',
+  },
+  {
+    id: '4',
+    title: '墙纸',
+  },
+  {
+    id: '5',
+    title: '地板',
+  },
+  {
+    id: '6',
+    title: '门窗',
+  },
+  {
+    id: '7',
+    title: '家电',
+  },
+  {
+    id: '8',
+    title: '浴霸',
+  },
+  {
+    id: '9',
+    title: '净水器',
+  },
+  {
+    id: '10',
+    title: '晾衣架',
+  },
+  {
+    id: '11',
+    title: '窗帘纱窗',
+  },
+  {
+    id: '12',
+    title: '集成吊顶',
+  },
+  {
+    id: '13',
+    title: '定制类',
+  },
+  {
+    id: '14',
+    title: '锁具',
+  },
+  {
+    id: '15',
+    title: '地毯',
+  },
+  {
+    id: '16',
+    title: '健身器材',
+  },
+];
+
+// 家居维修职业的服务类型
+const typeList = [
+  {
+    id: 10,
+    title: '安装',
+  },
+  {
+    id: 11,
+    title: '维修',
+  },
+  {
+    id: 12,
+    title: '送货',
+  },
+];
+
 export {
   careerList,
+  serviceCategoryList,
+  typeList,
 }
